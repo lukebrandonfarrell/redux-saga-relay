101e1ce5e6d1c5b3464e74483ee2574338a136fa
Luke Brandon Farrell <lukebrandonfarrell@gmail.com>
2019-06-03 13:55:34 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# redux-saga-relay
+
+NOTICE: This library is not ready for production. Use at your own discretion.
+
+## Authors
+
+* [**Luke Brandon Farrell**](https://lukebrandonfarrell.com/) - *Author*
+
+## License
+
+This project is licensed under the MIT Licen
---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * @author Luke Brandon Farrell
+ * @description
+ */
+
+import { call, put } from "redux-saga";
+import _omit from "lodash/omit";
+
+/**
+ * Relays an action to trigger another action
+ *
+ * @param type
+ * @param action
+ *
+ * @return {*}
+ */
+export function* relayAction(type, action) {
+  // We pull any analytics data out of the action when relayed
+  const parsedAction = _omit(action, ["meta.analytics"]);
+
+  yield put({
+    ...parsedAction,
+    type
+  });
+}
+
+/**
+ * Relays a saga to trigger a callback
+ *
+ * @param callback
+ * @param args
+ *
+ * @return {*}
+ */
+export function* relayCallback(callback, ...args) {
+  yield call(callback, ...args);
+}
